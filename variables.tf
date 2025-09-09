4e23d29611a73cc76a13589b62f5d48a406e810b
Justin Stricker <31423978+JustinStricker@users.noreply.github.com>
2025-09-08 22:27:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,45 @@
 variable "tenancy_ocid" {
+  type        = string
   description = "The OCID of the tenancy."
 }
 
 variable "user_ocid" {
+  type        = string
   description = "The OCID of the user."
 }
 
 variable "fingerprint" {
+  type        = string
   description = "The fingerprint of the API key."
 }
 
 variable "private_key" {
-  description = "The private key for the API."
+  type        = string
+  description = "The content of the private API key."
   sensitive   = true
 }
 
 variable "region" {
-  description = "The OCI region."
+  type        = string
+  description = "The OCI region to create resources in."
 }
 
 variable "compartment_ocid" {
-  description = "The OCID of the compartment."
+  type        = string
+  description = "The OCID of the compartment to create resources in."
+}
+
+variable "node_image_ocid" {
+  type        = string
+  description = "The OCID of the OKE node image to use."
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The full URL of the Docker image to deploy."
 }
 
 variable "tenancy_namespace" {
+  type = string
   description = "The OCI tenancy namespace for OCIR."
 }
