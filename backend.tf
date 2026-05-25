599ec8a3b853772a4f1438c748c5847739d0f9ab
justin <stric@duck.com>
2026-05-24 22:38:19 -0400

    fix: add use_path_style = true for OCI Object Storage S3 compatibility

--- backend.tf
+++ backend.tf
@@ -14,6 +14,7 @@ terraform {
     key                         = "infrastructure/terraform.tfstate"
     region                      = "us-ashburn-1"
     endpoint                    = "https://idrolupgk4or.compat.objectstorage.us-ashburn-1.oraclecloud.com"
+    use_path_style              = true
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
